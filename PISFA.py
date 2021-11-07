29f02b2e03afbf2a3f57b723d2f3caa3ffd4e4e1
OrnlyP63 <phiphat_c@cmu.ac.th>
2021-11-07 08:06:06 +0700

    GRP

--- PISFA.py
+++ PISFA.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 from sklearn.decomposition import PCA
+from sklearn.random_projection import GaussianRandomProjection
 #Error
 def _mean_squared_error(y, pred):
     return 0.5 * np.mean((y - pred) ** 2)
@@ -107,13 +108,14 @@ class PISFPA:
             x = (1-alpha)*Ty + alpha*Tz
 
             x0, x1 = x1, x
-
+            
+            
         if display_time:
             stop = time.time()
             print(f'Train time: {stop-start}')
 
         self._beta = x
-
+        
     def __call__(self, X):
         H = self._activation(X.dot(self._w) + self._bias)
         return H.dot(self._beta)
@@ -231,9 +233,73 @@ class PISFPA_PCA:
             print(f'Train time: {stop-start}')
 
         self._beta = x
-
+        
     def __call__(self, X):
         w = self.pca.transform(X)
+        H = self._activation(w + self._bias)
+        return H.dot(self._beta)
+
+    def evaluate(self, X, Y):
+        pred = self(X)
+
+        loss = self._loss(Y, pred)
+
+        acc = np.sum(np.argmax(pred, axis=-1) == np.argmax(Y, axis=-1)) / len(Y)
+
+        return loss, acc
+    
+    
+class PISFPA_GRP:
+    def __init__(self, num_input_nodes, num_hidden_units, num_output_units, activation='sigmoid',loss='mse', beta_init=None, w_init=None, bias_init=None):
+        self._num_input_nodes = num_input_nodes
+        self._num_hidden_units = num_hidden_units
+        self._num_output_units = num_output_units
+
+        self._activation = getActivation(activation)
+        self._loss = getLoss(loss)
+        self._beta = None
+        self._w = None
+        self.GRP = GaussianRandomProjection(n_components=num_hidden_units)
+        #weight_out
+        self._bias = np.random.uniform(-1., 1., size=(self._num_hidden_units,))
+
+    def fit(self, X, Y, itrs, lam, display_time=False):
+    
+        self._w = self.GRP.fit_transform(X)
+        H = self._activation(self._w + self._bias)
+        if display_time:
+            start = time.time()
+
+        L = 1. / np.max(np.linalg.eigvals(np.dot(H.T, H))).real
+        m = H.shape[1]
+        n = Y.shape[1]
+        x0 = np.zeros((m,n))
+        x1 = np.zeros((m,n))
+        L1 = 2*L*np.dot(H.T, H)
+        L2 = 2*L*np.dot(H.T, Y)
+
+        for i in range(1,itrs+1):
+            cn = ((2e-6*i)/(2*i+1))*lam*L
+            beta = 0.9*i/(i+1)
+            alpha = 0.9*i/(i+1)
+
+            y = x1 + thetan(x0,x1,i)*(x1-x0)
+            z = (1-beta)*x1 + beta*T(x1,L1,L2,cn)
+
+            Ty = T(y,L1,L2,cn)
+            Tz = T(z,L1,L2,cn)
+            x = (1-alpha)*Ty + alpha*Tz
+
+            x0, x1 = x1, x
+
+        if display_time:
+            stop = time.time()
+            print(f'Train time: {stop-start}')
+
+        self._beta = x
+        
+    def __call__(self, X):
+        w = self.GRP.transform(X)
         H = self._activation(w + self._bias)
         return H.dot(self._beta)
 
